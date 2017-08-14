53d0fd8785ef16e8974782ca28b0dba2366a55e9
xw <123@.com>
2017-08-14 17:10:23 +0800

    页面头部和尾部

---
+++ src/component/home/footer.vue
@@ -0,0 +1,36 @@
+<template>
+<footer class="footer">
+    <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item mui-active" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-extra mui-icon-extra-gift"></span>
+				<span class="mui-tab-label">商品购买</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-extra mui-icon-extra-express">
+                    <span class="mui-badge">0</span>
+                </span>
+				<span class="mui-tab-label">购物车</span>
+                
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon  mui-icon-contact"></span>
+				<span class="mui-tab-label">个人中心</span>
+			</a>
+		</nav>
+		<div></div>
+</footer>
+</template>
+<script>
+    
+</script>
+<style lang="less">
+    .footer{
+		div{
+			height:50px;
+		}
+	}
+</style>
---
+++ src/component/home/header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header class="header">
+      <mt-header fixed title="微商"></mt-header>
+      <div></div>
+  </header>
+</template>
+<script>
+    export default({
+
+    })
+</script>
+<style lang="less">
+     .mint-header {
+    background-color: #7ad2bc;
+  }
+  .header {
+    div {
+      height: 40px;
+    }
+  }
+</style>
